a16ad5d35cfb4e2aef937d821c4ea0feeed6bcb1
rxsegrxup <mrmchase08@gmail.com>
2013-08-27 17:55:47 -0400

    made link and name from nzb urlencoded

--- conf/notify.php
+++ conf/notify.php
@@ -1,21 +1,29 @@
 <?php
+session_start();
+ob_start();
 if(is_file("../conf/config.php")){
 	require("../conf/config.php");
 	if(isset($_REQUEST['link']) && isset($_REQUEST['name']) && isset($_REQUEST['method'])){
-		$l = escape_query($_REQUEST['link']);
-		$n = escape_query($_REQUEST['name']);
+		$l = urlencode($_REQUEST['link']);
+		$n = urlencode($_REQUEST['name']);
 		$md = escape_query($_REQUEST['method']);
 		$conf = new CONFIG;
-		if($md == "sab"){			
-			echo $conf->sendToSab($l, $n);
+		if($md == "sabnzbd"){			
+			$resp = $conf->sendToSab($l, $n);
+			$_SESSION['response'] = $resp;
 		}
 		elseif($md == "email"){
 			echo $conf->sendToMail($l, $n);
 		}
+		else{
+			$_SESSION['response']="No Method";
+		}
 	}
 	else{
-		echo "Error";
+		$_SESSION['response']="Error";
 	}
 }
+header("location: ../request/");
+ob_end_clean();
 exit;
 ?>
